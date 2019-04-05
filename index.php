f84dfbc9b7b3c68f387b2c35728476bc4bb6c5d8
Eda Gur <edagur14@ku.edu.tr>
2019-04-05 13:51:16 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php
 	echo "Messages Web Service";
-$servername = "http://eu-cdbr-west-02.cleardb.net";
+$servername = "eu-cdbr-west-02.cleardb.net";
 $username = "b7d8ffaef8f044";
 $password = "066e8219";
-//$dbname = "group2";
+$dbname = "heroku_529a72a5ae36523";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password);
+$conn = new mysqli($servername, $username, $password, $dbname);
 
 // Check connection
 if ($conn->connect_error) {
